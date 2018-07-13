deabdd10ec6d6e237634e0d9f1802b798e4469cb
huhulove <516378746@qq.com>
2018-07-13 17:09:22 +0800

    edit hbmap.js

--- hbmap/src/App.vue
+++ hbmap/src/App.vue
@@ -10,8 +10,8 @@
 
 <script>
 
-import hbmap from "./assets/hbmap.js"
-// import hbmap from "hbmap"
+// import hbmap from "./assets/hbmap.js"
+import hbmap from "hbmap"
 
 export default {
 
@@ -22,6 +22,7 @@ export default {
         return {
 
             map : null,
+            marker : null,
 
         }
 
@@ -43,13 +44,44 @@ export default {
 
         // hbmap.MapSwitchControl(this.map)
 
-        // hbmap.MapAddMarker( this.map )
+        this.marker = hbmap.MapAddMarker( this.map, 116.3569, 39.1234, false, true )
+
+        // hbmap.MapMarkerIsAnimate( this.marker, true )
 
         hbmap.MapAddCircle( this.map, 116.3569, 39.1234, 50000, {
             strokeColor : "red"
         } )
 
-        hbmap.MapAddPolygon(this.map, "116.3569, 39.1234; 117.3569, 39.4234; 117.3569, 38.4234;"  )
+        // hbmap.MapAddPolygon(this.map, "116.1234, 39.1234; 116.2341, 39.2234;", {}, true  )
+
+        //hbmap.MapSetScale( this.map, 8 )
+
+        hbmap.MapAddTextLabel( this.marker, "asdf", 116.3569, 39.1234, {width:300}, "block" );
+
+        // hbmap.MapAddBoundary(this.map, "山西省太原市万柏林区")
+
+        // hbmap.MapInfoWindow( this.map, {}, 116.3569, 39.4234 )
+
+        hbmap.MapRightMenu( this.marker, [{
+
+            text : "12234",
+            callback : () => {
+                alert(1)
+            }
+
+        }] )
+
+        hbmap.MapAddrToLngLat( "山西省太原市万柏林区和平南路16号", "太原市" ).then( (data) => {
+
+            console.log( data );
+
+        } )
+
+        hbmap.MapLngLatToAddr( hbmap.Point( 112.518016, 37.860284 ) ).then( (data) => {
+
+            console.log( data );
+
+        } )
 
     }
 
@@ -60,8 +92,8 @@ export default {
 <style scoped>
 
     #mapContainer{
-        width: 1000px;
-        height: 900px;
+        width: 800px;
+        height: 600px;
     }
 
 </style>
--- hbmap/src/assets/hbmap.js
+++ hbmap/src/assets/hbmap.js
@@ -58,13 +58,14 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-12
      * @version  [version]
-	 * @param    {[PointArr]}			   	点 实例 数组		       必填
-     * @return   {[object]}        	    	[线实例化对象]
+	 * @param    {[Object]}			   	PointArr			点 实例 数组		       必填
+	 * @param    {[Object]}				markerJson			标识物其他配置
+     * @return   {[Object]}        	    					[线实例化对象]
 	 * 
 	 */
-	Marker ( Point ){
+	Marker ( Point, markerJson ){
 
-		return new BMap.Marker( Point )
+		return markerJson ? new BMap.Marker( Point, markerJson ) : new BMap.Marker( Point )
 
 	},
 	/**
@@ -105,15 +106,171 @@ const hbmap = {
 	 */
 	Polygon ( points, stockStyle ){
 
-		var newStockStyle = {
+		return new BMap.Polygon( points, stockStyle );
 
-			strokeColor:"blue", 
-			strokeWeight:2, 
-			strokeOpacity:0.5,
-			...stockStyle
-		}
+	},
+	/**
+	 * 图标 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+	 * @param    {[String]}			iconUrl 		ICON 图标地址链接		       必填
+	 * @param    {[Object]}			size			尺寸实例化对象		       	   必填
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Icon (iconUrl, size){
 
-		return new BMap.Polygon( points, newStockStyle );
+		return new BMap.Icon( iconUrl, size )
+
+	},
+	/**
+	 * 尺寸 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+	 * @param    {[String]}			iconUrl 		ICON 图标地址链接		       必填
+	 * @param    {[Object]}			size			尺寸实例化对象		       	   必填
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Size ( num1, num2 ){
+
+		return new BMap.Size(num1, num2)
+
+	},
+	/**
+	 * 标注 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+	 * @param    {[String]}			iconUrl 		ICON 图标地址链接		       必填
+	 * @param    {[Object]}			size			尺寸实例化对象		       	   必填
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Label (text, opts){
+
+		return new BMap.Label(text, opts);
+
+	},
+	/**
+	 * 范围 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+	 * @param    {[String]}			iconUrl 		ICON 图标地址链接		       必填
+	 * @param    {[Object]}			size			尺寸实例化对象		       	   必填
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Boundary (){
+
+		return new BMap.Boundary()
+
+	},
+	/**
+	 * 信息窗口 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+	 * @param    {[String]}			text 			信息窗口所显示的文字		       必填
+	 * @param    {[Object]}			opts			信息窗口样式			       	   必填
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	InfoWindow (text, opts){
+
+		return new BMap.InfoWindow(text, opts)
+
+	},
+	/**
+	 * 右键菜单 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	ContextMenu (){
+
+		return new BMap.ContextMenu()
+
+	},
+	/**
+	 * 右键菜单项 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	ContextMenuItem (text, callback){
+
+		return new BMap.MenuItem(text, callback)
+
+	},
+	/**
+	 * 地址解析器 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Geocoder (){
+
+		return new BMap.Geocoder()
+
+	},
+	/**
+	 * 坐标转换器 实例
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-07-13
+     * @version  [version]
+     * @return   {[Object]}          	    		[ICON 实例化对象]
+	 * 
+	 */
+	Convertor (){
+
+		return new new BMap.Convertor()
+
+	},
+
+	
+
+	/**
+	 * 清除所有覆盖物
+	 * 
+	 * @Author   mayingwu
+     * @Email    516378746@qq.com
+     * @DateTime 2018-06-27
+     * @version  [version]
+	 * @param    {[mapObj]}			    地图实例化对象		       必填
+     * @return   {[type]}          	    [description]
+	 * 
+	 */
+	Remove (mapObj){
+
+		mapObj.clearOverlays();
 
 	},
 	
@@ -316,12 +473,37 @@ const hbmap = {
      * @version  [version]
 	 * @param    {[Object]}			  mapObj 			 [地图实例化对象]		       			必填
 	 * @param    {[Number]}			  lng 	             [经度]		       
-	 * @param    {[Number]}			  lat   			 [纬度]		       
+	 * @param    {[Number]}			  lat   			 [纬度]	
+	 * @param    {[Boolean]}		  isAnimate   		 [是否跳动]
+	 * @param    {[Boolean]}		  isDrag   		 	 [是否拖拽]
+	 * @param    {[Boolean/Object]}	  markerStyle   	 [自定义标识物样式]						true -> 默认的样式 / JSON -> 自定义样式		       
      * @return   {[Object]}          	    			 [标识物实例对象]
 	 */
-	MapAddMarker (mapObj, lng = 116.404, lat = 39.915){
+	MapAddMarker (mapObj, lng = 116.404, lat = 39.915, isAnimate = false, isDrag = false, markerStyle){
 		// 创建标识物
-		let marker = this.Marker( this.Point( lng, lat ) )
+		let marker = null;
+
+		if( markerStyle ){						// 自定义标识物样式
+				
+			let newMarkerStyle = {
+				icon : "http://lbsyun.baidu.com/jsdemo/img/fox.gif", 
+				width : 300, 
+				height : 157,
+				...markerStyle
+			}
+			let myIcon = this.Icon( newMarkerStyle.icon, this.Size( newMarkerStyle.width, newMarkerStyle.height ) );
+
+			marker = this.Marker( this.Point( lng, lat ), { icon: myIcon } );
+
+		}else{									// 默认标识物
+			marker = this.Marker( this.Point( lng, lat ) )
+		}
+		
+		// 标识物是否跳动
+		isAnimate ? marker.setAnimation( BMAP_ANIMATION_BOUNCE ) : "";
+		// 标识物是否拖拽
+		isDrag ? marker.enableDragging() : marker.disableDragging();
+
 		// 添加标识物
 		mapObj.addOverlay( marker )
 
@@ -344,15 +526,14 @@ const hbmap = {
 	MapAddCircle (mapObj, lng = 116.404, lat = 39.915, radius = 50000, stockStyle){
 		
 		let mapCircle = this.Circle( this.Point( lng, lat ), radius, stockStyle );
+		// 增加圆
+		mapObj.addOverlay(mapCircle);		
 
-		//增加圆
-		mapObj.addOverlay(mapCircle);
-		
 		return mapCircle
 
 	},
 	/**
-	 * 添加多边形
+	 * 添加多边形					？？？？？
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
@@ -364,36 +545,23 @@ const hbmap = {
 	 * 
 	 * 备注： pointArr => 116.404, 39.915; 116.504, 40.015;
 	 */
-	MapAddPolygon (mapObj, points, stockStyle){
+	MapAddPolygon (mapObj, points, stockStyle, isEdit = false){
 
-		let polygon = this.Polygon( points, stockStyle );
-		
-		//增加面
+		var newStockStyle = {
+
+			strokeColor:"blue", 
+			strokeWeight:2, 
+			strokeOpacity:0.5,
+			...stockStyle
+		}
+
+		let polygon = this.Polygon( points, newStockStyle );
+		// 是否编辑
+		isEdit ? polygon.enableEditing() : polygon.disableEditing()
+		// 增加面
 		mapObj.addOverlay(polygon);
 
 		return polygon
-
-	},
-
-
-	/////////////////////////////////////
-
-
-
-	/**
-	 * 清除覆盖物
-	 * 
-	 * @Author   mayingwu
-     * @Email    516378746@qq.com
-     * @DateTime 2018-06-27
-     * @version  [version]
-	 * @param    {[mapObj]}			    地图实例化对象		       必填
-     * @return   {[type]}          	    [description]
-	 * 
-	 */
-	MapRemove (mapObj){
-
-		mapObj.clearOverlays();
 
 	},
 	/**
@@ -403,9 +571,9 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-06-27
      * @version  [version]
-	 * @param    {[mapObj]}			    地图实例化对象		           必填
-	 * @param    {[num]}			    地图放大/缩放级别		       必填
-     * @return   {[type]}          	    [description]
+	 * @param    {[Object]}			mapObj			    地图实例化对象		           必填
+	 * @param    {[Number]}			num				    地图放大/缩放级别		       必填
+     * @return   {[type]}          	    				[description]
 	 * 
 	 */
 	MapSetScale (mapObj, num){
@@ -414,91 +582,51 @@ const hbmap = {
 
 	},
 	/**
-	 * 点的弹跳动画
+	 * 标识物显示 / 隐藏
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
      * @DateTime 2018-06-27
      * @version  [version]
-	 * @param    {[markerObj]}			    遮罩层实例化对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			markerObj			标识物实例对象		       必填
+     * @return   {[type]}          	    				[description]
 	 * 
 	 */
-	MapPointAnimate (markerObj){
+	MapMarkerIsShow (markerObj, isShow){
 
-		markerObj.setAnimation( BMAP_ANIMATION_BOUNCE )
+		isShow ? markerObj.show() : markerObj.hide();
 
 	},
 	/**
-	 * 点的新图标样式
+	 * 标识物是否拖拽
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
-     * @DateTime 2018-06-27
+     * @DateTime 2018-07-13
      * @version  [version]
-	 * @param    {[markerObj]}			    遮罩层实例化对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			markerObj			标识物实例对象		       必填
+     * @return   {[type]}          	    				[description]
 	 * 
 	 */
-	MapPointStyle (mapObj, lng = 116.404, lat = 39.915 ){
-
-		let pt = new BMap.Point( lng, lat )
-
-		let myIcon = new BMap.Icon( "http://lbsyun.baidu.com/jsdemo/img/fox.gif", new BMap.Size(300, 157) );
-
-		let marker2 = new BMap.Marker( pt, {icon: myIcon} );
-
-		mapObj.addOverlay( marker2 )
-
-		return marker2;
-
-	},
-	/**
-	 * 点是否可拖拽
-	 * 
-	 * @Author   mayingwu
-     * @Email    516378746@qq.com
-     * @DateTime 2018-06-27
-     * @version  [version]
-	 * @param    {[markerObj]}			    遮罩层实例化对象		       必填
-     * @return   {[type]}          	    	[description]
-	 * 
-	 */
-	MapPointDrag (markerObj, isDrag = false){
+	MapMarkerIsDrag (markerObj, isDrag){
 
 		isDrag ? markerObj.enableDragging() : markerObj.disableDragging();
 
 	},
 	/**
-	 * 是否可编辑线 / 面
+	 * 标识物是否跳动
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
-     * @DateTime 2018-06-27
+     * @DateTime 2018-07-13
      * @version  [version]
-	 * @param    {[obj]}			    	线 / 面实例化对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			markerObj			标识物实例对象		       必填
+     * @return   {[type]}          	    				[description]
 	 * 
 	 */
-	MapEditPointSide (obj, isEdit){
+	MapMarkerIsAnimate (markerObj, isAnimate){
 
-		isEdit ? obj.enableEditing() : obj.disableEditing()
-
-	},
-	/**
-	 * 是否显示 / 隐藏
-	 * 
-	 * @Author   mayingwu
-     * @Email    516378746@qq.com
-     * @DateTime 2018-06-27
-     * @version  [version]
-	 * @param    {[markerObj]}			    遮罩层实例对象		       必填
-     * @return   {[type]}          	    	[description]
-	 * 
-	 */
-	MapMarkerIsShow (markerObj, IsShow){
-
-		IsShow ? markerObj.show() : markerObj.hide();
+		isAnimate ? markerObj.setAnimation( BMAP_ANIMATION_BOUNCE ) : "";
 
 	},
 	/**
@@ -508,12 +636,13 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-06-27
      * @version  [version]
-	 * @param    {[markerObj]}			   	遮罩层实例对象		       		必填
-	 * @param    {[lng]}			    	经度					       必填
-	 * @param    {[lat]}			    	纬度		    			   必填
-	 * @param    {[labelStyle]}			    label 样式
-	 * @param    {[isShow]}			    	label 是否显示			
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			markerObj		   	标识物实例对象		       		必填
+	 * @param    {[String]}			text		    	经度					       必填
+	 * @param    {[Number]}			lng			    	经度					       
+	 * @param    {[Number]}			lat			    	纬度		    			   
+	 * @param    {[Object]}			labelStyle		    label 样式
+	 * @param    {[Boolean]}		isShow		    	label 是否显示			
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 * 备注 ： isShow => "block" / "none"
 	 */
@@ -533,13 +662,13 @@ const hbmap = {
 
 		let opts = {
 			// 指定文本标注所在的地理位置
-			position : new BMap.Point(lng, lat),
+			position : this.Point(lng, lat),
 			//设置文本偏移量					
-			offset   : new BMap.Size(lStyle.left, lStyle.top)    	
+			offset   : this.Size(lStyle.left, lStyle.top)    	
 		}
 		
 		// 创建文本标注对象
-		let label = new BMap.Label(text, opts);  
+		let label = this.Label(text, opts);  
 
 		label.setStyle({
 			"display" : isShow,
@@ -550,15 +679,15 @@ const hbmap = {
 
 	},
 	/**
-	 * 删除特定点
+	 * 根据标注文字删除特定点
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
      * @DateTime 2018-06-27
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图实例对象	 		        必填
-	 * @param    {[labelText]}			    label 文字内容
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			mapObj		   		地图实例对象	 		        必填
+	 * @param    {[String]}			labelText		    label 文字内容
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
 	MapDeletePoint (mapObj, labelText){
@@ -579,14 +708,14 @@ const hbmap = {
 
 	},
 	/**
-	 * 获取覆盖物信息
+	 * 获取标识物信息
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
      * @DateTime 2018-06-28
      * @version  [version]
-	 * @param    {[markerObj]}			   	遮罩层实例对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			markerObj		   	遮罩层实例对象		       必填
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
 	MapMarkerLngLat (markerObj){
@@ -595,15 +724,15 @@ const hbmap = {
 
 	},
 	/**
-	 * 移除覆盖物信息
+	 * 移除标识物信息
 	 * 
 	 * @Author   mayingwu
      * @Email    516378746@qq.com
      * @DateTime 2018-07-02
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图实例对象		       必填
-	 * @param    {[markerObj]}			   	遮罩层实例对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			mapObj		   		地图实例对象		       必填
+	 * @param    {[Object]}			markerObj		   	遮罩层实例对象		       必填
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
 	MapMarkerRemove (mapObj, markerObj){
@@ -618,17 +747,18 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-06-28
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图实例对象		       必填
-     * @return   {[type]}          	    	[description]
+	 * @param    {[Object]}			mapObj		   		地图实例对象		       必填
+	 * @param	 {[String]}			boundary			行政区					   必填		
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
-	MapAddBoundary (mapObj){
+	MapAddBoundary (mapObj, boundary){
 
-		let bdary = new BMap.Boundary();
+		let bdary = this.Boundary();
 		//获取行政区域
-		bdary.get("山西省太原市晋源区", (rs) => {       
+		bdary.get(boundary, (rs) => {       
 			//清除地图覆盖物
-			mapObj.clearOverlays();         
+			this.Remove(mapObj);         
 			//行政区域的点有多少个
 			let count = rs.boundaries.length;
 
@@ -643,7 +773,7 @@ const hbmap = {
 			  
 			for (let i = 0; i < count; i++) {
 				//建立多边形覆盖物
-				let ply = new BMap.Polygon(rs.boundaries[i]);
+				let ply = this.Polygon(rs.boundaries[i]);
 
 				pointsStr += rs.boundaries[i]
 
@@ -659,7 +789,6 @@ const hbmap = {
 		});   
 
 	},
-	
 	/**
 	 * 信息窗口
 	 * 
@@ -667,11 +796,14 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-02
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图实例对象		       必填
-     * @return   {[object]}          	    [信息窗口实例化对象]
+	 * @param    {[Object]}			mapObj		   		地图实例对象		       必填
+	 * @param    {[Object]}			infoOpt		   		信息窗口样式		       必填
+	 * @param    {[Number]}			lng				   	经度				       必填
+	 * @param    {[Number]}			lat			   		纬度				       必填
+     * @return   {[object]}          	    			[信息窗口实例化对象]
 	 * 
 	 */
-	MapInfoWindow (mapObj, infoOpt, target){
+	MapInfoWindow (mapObj, infoOpt, lng, lat){
 
 		let opts = {
 			width : 200,     	
@@ -683,9 +815,9 @@ const hbmap = {
 			...infoOpt
 		}
 		// 创建信息窗口对象 
-		let infoWindow = new BMap.InfoWindow( opts.content, opts );  
+		let infoWindow = this.InfoWindow( opts.content, opts );  
 		//开启信息窗口
-		mapObj.openInfoWindow(infoWindow, target); 
+		mapObj.openInfoWindow(infoWindow, this.Point(lng, lat)); 
 
 		return infoWindow
 
@@ -697,21 +829,22 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-02
      * @version  [version]
-	 * @param    {[Obj]}			   		地图/覆盖物 实例对象		       必填
-     * @return   {[object]}          	    [信息窗口实例化对象]
+	 * @param    {[Object]}			 Obj			   	 地图/覆盖物 实例对象		  必填
+	 * @param	 {[Array]}			 menuItem			 菜单项数组  [ {text : "123", callback : function(){alert(1)}} ]
+     * @return   {[type]}          	    				 [undefined]
 	 * 
 	 */
 	MapRightMenu (Obj, menuItem){
 
-		let menu = new BMap.ContextMenu();
+		let menu = this.ContextMenu();
 
 		let txtMenuItem = [
 			...menuItem
 		];
-
+		
 		for(let i=0; i < txtMenuItem.length; i++){
 
-			menu.addItem( new BMap.MenuItem( txtMenuItem[i].text, txtMenuItem[i].callback, 100 ) );
+			menu.addItem( this.ContextMenuItem( txtMenuItem[i].text, txtMenuItem[i].callback, 100 ) );
 
 		}
 
@@ -725,9 +858,9 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-03
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图 实例对象		       必填
-	 * @param    {[url]}			   		鼠标样式图片链接地址       必填
-     * @return   {[object]}          	    [信息窗口实例化对象]
+	 * @param    {[Object]}			mapObj		   		地图 实例对象		       必填
+	 * @param    {[String]}			url			   		鼠标样式图片链接地址       必填
+     * @return   {[type]}          	    			[undefined]
 	 * 
 	 */
 	MapSetMouseStyle (mapObj, url){
@@ -742,9 +875,9 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-03
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图 实例对象		       必填
-	 * @param    {[fn]}			   			回调函数			       必填
-     * @return   {[object]}          	    [信息窗口实例化对象]
+	 * @param    {[Object]}			mapObj		   		地图 实例对象		       必填
+	 * @param    {[Function]}		fn		   			回调函数			       必填
+     * @return   {[type]}          				    [undefined]
 	 * 
 	 */
 	MapReady (mapObj, fn){
@@ -763,8 +896,10 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-03
      * @version  [version]
-	 * @param    {[obj]}			   		地图/覆盖物 实例对象		       必填
-     * @return   {[object]}          	    [信息窗口实例化对象]
+	 * @param    {[Object]}			obj			   		地图/覆盖物 实例对象		       必填
+	 * @param    {[String]}			eventName		    事件名字						  必填
+	 * @param    {[String]}         eventFn				函数名字						  必填
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
 	MapRemoveEvent (obj, eventName, eventFn){
@@ -779,9 +914,9 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-03
      * @version  [version]
-	 * @param    {[addr]}			   		详细地址		    			   必填
-	 * @param    {[city]}			   		详细地址对应所在城市		       必填
-     * @return   {[]}          	    []
+	 * @param    {[String]}			addr		   		详细地址		    			   必填
+	 * @param    {[String]}			city		   		详细地址对应所在城市		       必填
+     * @return   {[type]}          	    				[undefined]
 	 * 
 	 */
 	MapAddrToLngLat (addr, city){
@@ -789,7 +924,7 @@ const hbmap = {
 		return new Promise( (resolve, reject) => {
 
 			// 创建地址解析器实例
-			let myGeo = new BMap.Geocoder();
+			let myGeo = this.Geocoder();
 
 			// 将地址解析结果显示在地图上,并调整地图视野
 			myGeo.getPoint(addr, (point) => {
@@ -816,14 +951,14 @@ const hbmap = {
      * @Email    516378746@qq.com
      * @DateTime 2018-07-03
      * @version  [version]
-	 * @param    {[mapObj]}			   		地图 实例对象		       必填
+	 * @param    {[Object]}				point			   		点 实例对象		       必填
 	 * 
 	 */
 	MapLngLatToAddr (point){
 
 		return new Promise( (resolve, reject) => {
 
-			var geoc = new BMap.Geocoder();
+			var geoc = this.Geocoder();
 
 			geoc.getLocation(point, function(rs){
 				
@@ -849,9 +984,9 @@ const hbmap = {
 
 		return new Promise( (resolve, reject) => {
 
-			var ggPoint = new BMap.Point(x,y);
+			var ggPoint = this.Point(x,y);
 
-			var convertor = new BMap.Convertor();
+			var convertor = this.Convertor();
 
 			var pointArr = [];
 
